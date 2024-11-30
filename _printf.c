e5cd0b95212f0a6ff5915353a747a3f18c2bb68d
Yoruan <yoruanorta50@gmail.com>
2024-11-30 15:43:52 -0400

    The true printf

--- _printf.c
+++ _printf.c
@@ -7,63 +7,27 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char *str;
+	va_list args;
+	int i = 0, count = 0;
 
-    if (!format)
-        return (-1);
+	if (!format)
+	return (-1);
 
-    va_start(args, format);
-    while (format[i])
-    {
-        if (format[i] == '%' && format[i + 1])
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'd':
-                case 'i':
-                    count += printf("%d", va_arg(args, int));
-                    break;
-		case 'u':
-		    count += printf("%u", va_arg(args, int));
-		    break;
-		case 'o':
-		    count += printf("%o", va_arg(args, unsigned int));
-		    break;
-		case 'x':
-		    count += printf("%x", va_arg(args, unsigned int));
-		    break;
-		case 'X':
-		    count += printf("%X", va_arg(args, unsigned int));
-		    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    count += printf("%s", str ? str : "(null)");
-                    break;
-                case 'c':
-                    count += printf("%c", va_arg(args, int));
-                    break;
-		case 'p':
-		    count += printf("%p", va_arg(args, void *));
-		    break;
-                case '%':
-                    count += printf("%%");
-                    break;
-                default:
-                    putchar('%');
-		    putchar(format[i]);
-		    count += 2;
-            }
-        }
-        else
-        {
-            putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			count += manage_specifier(format[i], args);
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
